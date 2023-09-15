fa25c7404b8b67927e58a95ccd272ac95fbcbba2
J Read <j@8bit.coffee>
2023-09-15 17:28:27 -0500

    complete. Added comments, polished CSS, added footer and icon

--- salaryCalculator.js
+++ salaryCalculator.js
@@ -14,26 +14,28 @@ let monthlyWages = Math.round(65000/12); // Set with example value. Always kept 
  */
 function addEmployee(event){
 
-    event.preventDefault();
+    event.preventDefault(); // Prevents page refresh
 
-    let firstName = document.querySelector('#first-name').value;
-    firstName == "" ? firstName = "Peter" : null;
-    let lastName = document.querySelector('#last-name').value;
-    lastName == "" ? lastName = "Venkman" : null;
-    let id = document.querySelector('#id-number').value;
-    id == "" ? id = "ECTO-1" : null;
-    let jobTitle = document.querySelector('#job-title').value;
-    jobTitle == "" ? jobTitle = "Ghostbuster" : null;
-    let annualSalary = document.querySelector('#annual-salary').value;
+    let firstName = document.querySelector('#first-name').value; // First name input field
+    firstName == "" ? firstName = "Peter" : null; // default value
+    let lastName = document.querySelector('#last-name').value; // Last name input field
+    lastName == "" ? lastName = "Venkman" : null; // default value
+    let id = document.querySelector('#id-number').value; // id input field
+    id == "" ? id = "ECTO-1" : null; // default value
+    let jobTitle = document.querySelector('#job-title').value; // job title input field
+    jobTitle == "" ? jobTitle = "Ghostbuster" : null; // default value
+    let annualSalary = document.querySelector('#annual-salary').value; // annual salary input field
 
+    // removes cents if included in salary
     if (annualSalary.includes('.')){
-        annualSalary = annualSalary.replace(/\.\d+/,'');
+        annualSalary = annualSalary.replace(/\.\d+/,''); // Regex that eliminates the decimal and the following when
+                                                             // the decimal is at the end followed by 2 numbers
     }
 
-    let tempstr = '';
-
+    // removes everything except numbers if included in salary. Returns as number instead of string.
     annualSalary = stripFormat(annualSalary);
 
+    // Adds input field info to table. annualSalary is formated with commas.
     tableBody.innerHTML += `
         <tr>
             <td>${firstName}</td>
@@ -45,46 +47,70 @@ function addEmployee(event){
         </tr>
     `;
 
+    // Clears all input fields
     document.querySelector('#first-name').value = "";
     document.querySelector('#last-name').value = "";
     document.querySelector('#id-number').value = "";
     document.querySelector('#job-title').value = "";
     document.querySelector('#annual-salary').value = "";
 
+    // updates the monthly wages total on DOM
     return updateTotal(annualSalary);
 }
 
+/**
+ * Removes employee div from table. Pulls that employee annual salary from target and
+ *      and passes it to updateTotal() as a negative number to remove that amount from monthly total.
+ * @param {Object} event // event object returned by onClick 
+ * @returns Passes to updateTotal as negative number.
+ */
 function removeEmployee(event){
     event.target.parentElement.parentElement.remove();
     let annualSalary = event.target.parentElement.parentElement.children[4].innerHTML;
-    annualSalary = stripFormat(annualSalary);
+    annualSalary = stripFormat(annualSalary); // returns as stripped format in number instead of string
     return updateTotal(0 - annualSalary);
 }
 
+/**
+ * Updates monthly total on DOM as formatted string
+ * @param {Number} annualSalary 
+ */
 function updateTotal(annualSalary){
-    monthlyWages += Math.round(annualSalary / 12);
+    monthlyWages += Math.round(annualSalary / 12); // Converts to monthly from annual
     monthlyTotal.innerHTML = `
         Total monthly wages: $${formatCash(monthlyWages)}
     `;
 
 }
 
+/**
+ * Takes number and returns as formatted string w/commas
+ * @param {Number} salary 
+ * @returns {String} formatted number
+ */
 function formatCash(salary){
     salary = Number(salary);
     let formattedSalary = salary.toLocaleString('en-US');
     return String(formattedSalary);
 }
 
+/**
+ * Takes a string that hopefully contains some numbers. Strips anything except numbers
+ * @param {String} fancyNumberString // String with commas and/or symbols
+ * @returns {Number} Just the numbers in the same order and as a number.
+ *                      Empty string returns as Number('0')
+ */
 function stripFormat(fancyNumberString){
-    let tempstr = '';
+    
+    let tempstr = ''; // empty string that will collect the numbers
 
     for (let i in fancyNumberString){
-        for (let x in [...Array(10).keys()]){
+        for (let x in [...Array(10).keys()]){ // creates and iterates over an array of numbers 0-9
             if (fancyNumberString[i] == x){
-                tempstr += fancyNumberString[i];
+                tempstr += fancyNumberString[i]; // If the single character is a number, added to tempstr
             }
         }
     }
 
-    return Number(tempstr);
+    return Number(tempstr); // returns the collected numbers in order as a number
 }
